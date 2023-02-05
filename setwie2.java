b78721b1dd09bf376dc78194a5ede64bd95b6a64
AnisPartovov <115348097+AnisPartovov@users.noreply.github.com>
2023-02-05 22:29:27 +0600

    Add files via upload

---
+++ setwie2.java
@@ -0,0 +1,14 @@
+public class setwie2 {
+    public static void main(String[] args) {
+        slash();
+        for (int i = 0; i < 5; i++) victory();
+    }
+    public static void slash() {
+        System.out.println("//////////////////////");
+    }
+
+    public static void victory() {
+        System.out.println("|| Victory is mine! ||");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+    }
+}
